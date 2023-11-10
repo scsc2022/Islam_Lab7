f810d51fe9dcabcd780614e9529dae8d785c11d5
scsc2022 <schararaislam13@gmail.com>
2023-11-10 13:19:37 -0500

    created the local variable of _units and _rate

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -1,6 +1,8 @@
 public class LifelineSite extends Site {
 
     public double getBillableAmount(){
+        double _units;
+        double _rate;
         double base = _units * _rate *0.5;
         double tax = base* Site.Tax_rate *0.2;
         return base+ tax;
--- src/ResidentialSite.java
+++ src/ResidentialSite.java
@@ -1,5 +1,7 @@
 public class ResidentialSite extends Site {
     public double getBillableAmount(){
+        double _units;
+        double _rate;
         double base = _units * _rate;
         double tax = base* Site.Tax_rate;
         return base+ tax;
