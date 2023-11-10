a327629f70298d91a764994e023f2cc629c07f45
scsc2022 <schararaislam13@gmail.com>
2023-11-10 13:31:23 -0500

    Edied the getBillableAmount and rename the private method

--- src/ResidentialSite.java
+++ src/ResidentialSite.java
@@ -1,8 +1,16 @@
 public class ResidentialSite extends Site {
-    public double getBillableAmount(){
-        double _units;
-        double _rate;
-        double base = _units * _rate;
+    private double units;
+    private double rate;
+    private double Tax_rate;
+
+    public ResidentialSite(){
+        this.units = units;
+        this.rate = rate;
+        this.Tax_rate = Site.Tax_rate;
+    }
+
+    public double getBillableAmount(double units, double rate){
+        double base = units * rate;
         double tax = base* Site.Tax_rate;
         return base+ tax;
     }
