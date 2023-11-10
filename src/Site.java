8a26aef1f6f32c320a81855f3ebed7c0eb51a155
scsc2022 <schararaislam13@gmail.com>
2023-11-10 13:44:25 -0500

    rename the function to make it easier

--- src/Site.java
+++ src/Site.java
@@ -1,3 +1,13 @@
-public class Site {
-    protected static double Tax_rate;
+public abstract class Site {
+    protected static double Tax_rate = 0.12;
+    abstract double getBaseAmount();
+
+    abstract double getTaxAmount();
+
+    public double getBillableAmount(){
+        double base = getBaseAmount();
+        double tax = getTaxAmount();
+        return base+tax;
+    }
+
 }
