f2c9dfae18ce3a369158383e739828f477a75845
scsc2022 <schararaislam13@gmail.com>
2023-11-10 13:25:30 -0500

    rename the private method and added a public constructor

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -1,8 +1,19 @@
 public class LifelineSite extends Site {
 
+    protected double base;
+    private int _units;
+    private static int _rate;
+    private Site site;
+
+    /**
+     * Public contructor
+     */
+    public LifelineSite(){
+        this._units = _units;
+        this._rate = _rate;
+    }
+
     public double getBillableAmount(){
-        double _units;
-        double _rate;
         double base = _units * _rate *0.5;
         double tax = base* Site.Tax_rate *0.2;
         return base+ tax;
