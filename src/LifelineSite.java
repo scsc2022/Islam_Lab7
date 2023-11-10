8958207f82707229ecafc1080d243c847866b3d2
scsc2022 <schararaislam13@gmail.com>
2023-11-10 13:45:05 -0500

    created the function that allows to showcase base amount and tax amount

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -1,13 +1,10 @@
 public class LifelineSite extends Site {
-
+/**
     protected double base;
     private int units;
     private static int rate;
     private Site site;
 
-    /**
-     * Public contructor
-     */
     public LifelineSite(){
         this.units = units;
         this.rate = rate;
@@ -17,5 +14,19 @@ public class LifelineSite extends Site {
         double base = units * LifelineSite.rate *0.5;
         double tax = base* site.Tax_rate *0.2;
         return base + tax;
+    }
+    */
+
+    private double units;
+    private double rate;
+
+    @Override
+    public double getBaseAmount(){
+        return units*rate;
+    }
+
+    @Override
+    public double getTaxAmount(){
+        return getBaseAmount() * Site.Tax_rate;
     }
 }
--- src/ResidentialSite.java
+++ src/ResidentialSite.java
@@ -1,4 +1,5 @@
 public class ResidentialSite extends Site {
+    /**
     private double units;
     private double rate;
     private double Tax_rate;
@@ -13,5 +14,18 @@ public class ResidentialSite extends Site {
         double base = units * rate;
         double tax = base* Site.Tax_rate;
         return base+ tax;
+    }
+     */
+    private double units;
+    private double rate;
+
+    @Override
+    public double getBaseAmount(){
+        return units*rate*0.5;
+    }
+
+    @Override
+    public double getTaxAmount(){
+        return getBaseAmount() * Site.Tax_rate *0.2;
     }
 }
