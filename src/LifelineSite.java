2bd2cfa4d199d2a1d08ba5df6068bb9ab8d8494a
scsc2022 <schararaislam13@gmail.com>
2023-11-10 13:28:16 -0500

    Edied the getBillableAmount

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -1,21 +1,21 @@
 public class LifelineSite extends Site {
 
     protected double base;
-    private int _units;
-    private static int _rate;
+    private int units;
+    private static int rate;
     private Site site;
 
     /**
      * Public contructor
      */
     public LifelineSite(){
-        this._units = _units;
-        this._rate = _rate;
+        this.units = units;
+        this.rate = rate;
     }
 
-    public double getBillableAmount(){
-        double base = _units * _rate *0.5;
-        double tax = base* Site.Tax_rate *0.2;
-        return base+ tax;
+    public double getBillableAmount(double units, double rate){
+        double base = units * LifelineSite.rate *0.5;
+        double tax = base* site.Tax_rate *0.2;
+        return base + tax;
     }
 }
